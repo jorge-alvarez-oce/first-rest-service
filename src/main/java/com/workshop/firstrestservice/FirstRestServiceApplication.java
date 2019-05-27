274032cbe9ba546bd2d04779ac860ec1d947e88f
Jorge Luis Alvarez Alvarez <george.alvarez.oce@gmail.com>
2019-05-27 00:38:52 -0500

    feat: Add scaffolding

---
+++ src/main/java/com/workshop/firstrestservice/FirstRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.firstrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestServiceApplication.class, args);
+	}
+
+}
